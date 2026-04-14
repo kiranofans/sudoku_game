bbe25cc53d63566d98daa32f00498b8edbfe40a1
Shu <sakurala111@yahoo.com>
2026-04-14 03:46:00 -0700

    Feat: implemented contact us feature with EmailJs

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '@/components/Layout';
+import emailjs from '@emailjs/browser';
+
+interface ContactFormData {
+    email: string;
+    subject: string;
+    message: string;
+}
+const sendEmail = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const data: ContactFormData = {
+        email: formData.get('email') as string,
+        subject: formData.get('subject') as string,
+        message: formData.get('message') as string,
+    };
+    try {
+        const response = await emailjs.send(
+            "service_6r35nck",
+            "template_h0k37j5",
+            {
+                user_email: data.email,
+                subject: data.subject,
+                message: data.message
+            },
+            "6iWrJ5lrgdzAgyWGp"
+        );
+        console.log("SUCESS!", response.status, response.text);
+        alert("Email sent successfully");
+        form.reset();
+    } catch (err) {
+        console.error('FAILED...', err);
+        alert('Failed to send email');
+    };
+
+    //Attachment to existing form
+    // const formElement = document.getElementById('contact-form') as HTMLFormElement;
+    // formElement.addEventListener("submit", {sendEmail});
+}
 
 const Contact: React.FC = () => {
     return (
@@ -8,9 +47,43 @@ const Contact: React.FC = () => {
             <main className="sudoku-app" style={{ width: '100%', padding: '6rem 2rem 6rem', maxWidth: '900px', textAlign: 'left', margin: '0 auto', flex: '1 0 auto' }}>
                 <h1 className="text-3xl font-bold text-center mt-6 mb-8 dark:text-white">Contact</h1>
 
-                <div style={{ textAlign: 'center', padding: '2rem 0', fontSize: '1.2rem', lineHeight: '1.6' }} className="dark:text-gray-200">
-                    <p style={{ marginBottom: '1.5rem' }}>For support or feedback, please contact us at:</p>
-                    <a
+                <div style={{ textAlign: 'center', padding: '1.5rem 0', fontSize: '1.2rem', lineHeight: '1.6' }}
+                    className="dark:text-gray-200 w-2/3">
+                    <form id="contact-form" onSubmit={sendEmail}>
+                        <div className='felex flex-col'>
+                            <div className='flex flex-col text-left' style={{ marginBottom: '1.5rem' }}>
+                                <label htmlFor="email" style={{ marginBottom: '0.5rem' }}><h3>Your Email Address: </h3></label>
+                                <input className='border-gray-400 dark:bg-gray-800 dark:text-blue-400 
+                                    dark:border-gray-700 border-2 rounded-md p-2 h-10'
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    placeholder="you@example.com"
+                                    required
+                                /></div>
+                            <div className='flex flex-col text-left' style={{ marginBottom: '1.5rem' }}>
+
+                                <label htmlFor="subject" className='text-md'>The Subject: </label>
+
+                                <input type="subject" id="subject" name="subject" style={{ marginTop: '0.5rem' }}
+                                    className='border-gray-400 dark:bg-gray-800 dark:text-blue-400 
+                                    dark:border-gray-700 border-2 rounded-md p-2 h-10'
+                                    placeholder="What is this about?" />
+                            </div>
+
+                            <div className='flex flex-col text-left' style={{ marginBottom: '1rem' }}>
+                                <label htmlFor="message" style={{ marginBottom: '0.5rem' }}>Your Message</label>
+                                <textarea className="border-gray-400 dark:bg-gray-800 dark:text-blue-400 dark:border-gray-700 border-2 rounded-md p-2 h-50"
+                                    id="message" name="message" placeholder="What is your idea about in detail?" required />
+                            </div>
+                            <button type="submit" className='new-game-btn'
+                                style={{ textDecoration: 'none', display: 'inline-block', marginTop: '1.5rem' }}>
+                                Submit</button>
+                        </div>
+                    </form>
+
+                    {/* <p style={{ marginBottom: '0.1rem', marginTop: '1.5rem' }}>For support or feedback, please contact us at:</p> */}
+                    {/* <a
                         href="mailto:sitehelp.chat@gmail.com"
                         className="dark:bg-gray-800 dark:text-blue-400 dark:border-gray-700"
                         style={{
@@ -27,18 +100,18 @@ const Contact: React.FC = () => {
                         }}
                     >
                         sitehelp.chat@gmail.com
-                    </a>
+                    </a> */}
 
-                    <p style={{ marginTop: '3.5rem', fontSize: '1rem' }} className="text-gray-600 dark:text-gray-400">
+                    {/* <p style={{ marginTop: '3.5rem', fontSize: '1rem' }} className="text-gray-600 dark:text-gray-400">
                         We value your input and strive to respond to all inquiries as quickly as possible.
-                    </p>
+                    </p> */}
                 </div>
 
-                <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+                {/* <div style={{ textAlign: 'center', marginTop: '1rem' }}>
                     <Link to="/" className="new-game-btn" style={{ textDecoration: 'none', display: 'inline-block' }}>
                         Back to Game
                     </Link>
-                </div>
+                </div> */}
             </main>
         </Layout>
     );
