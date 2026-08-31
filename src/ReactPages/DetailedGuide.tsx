6b499e9489d6b145d59eaeec4a6a0a5014edd844
Shu <sakurala111@yahoo.com>
2026-08-30 23:46:12 -0700

    UI: add underconstruction for how sudokuplays works

--- src/ReactPages/DetailedGuide.tsx
+++ src/ReactPages/DetailedGuide.tsx
@@ -1,5 +1,26 @@
+import Layout from '@/components/Layout';
+import { ThemeProvider } from '@/components/ThemeContext';
 
 function DetailedGuide() {
+    return (
+        <ThemeProvider>
+            <Layout>
+                <main className='sudoku-app' style={{ width: '100%', padding: '6rem 2rem 6rem', maxWidth: '900px', textAlign: 'left', flex: '1 0 auto' }}>
+                    <h1 className="text-3xl font-bold text-center mt-6 mb-6 dark:text-white">How SudokuPlays Works</h1>
 
+                    <div className="">
+                        <p className="text-xl text-center justify-center items-center">
+                            Under Construction
+                        </p>
+                        <img
+                            className="w-200 h-140"
+                            src="/images/png/under-construction.png"
+                            alt="Under Construction"
+                        />
+                    </div>
+                </main>
+            </Layout>
+        </ThemeProvider>
+    );
 }
 export default DetailedGuide;
